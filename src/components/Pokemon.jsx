53a109a87ea1cf1a9a4faf6122200f892bf80d6d
Diego Díaz Morón <alu0101337975@ull.edu.es>
2023-10-15 22:37:32 +0100

    onClick function added

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,14 +1,24 @@
+import { useState } from "react";
+
 function PokemonNoResults() {
   return <div>No results</div>;
 }
 
 function ListOfPokemons({ pokemons }) {
+  const [click, setClick] = useState(false);
+
+  const handleClick = (event) => {
+    setClick(!click);
+    console.log(event.target);
+  };
+
   return (
     <ul className="pokemons">
       {pokemons.map((pokemon) => (
-        <li className="pokemon" key={pokemon.id}>
+        <li className="pokemon" key={pokemon.id} onClick={handleClick}>
           <h2>{pokemon.name}</h2>
           <img src={pokemon.image} alt={pokemon.name} />
+          {click && <p>{"#" + pokemon.id}</p>}
         </li>
       ))}
     </ul>
